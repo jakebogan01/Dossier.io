fb5810bdf7deaf06a903860917f199ef8f2d1164
Jake Bogan <jakebogan01@gmail.com>
2022-05-17 00:48:22 -0400

    added browser sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,30 @@
 const mix = require('laravel-mix');
+const domain = 'dossier.test';
+const homedir = require('os').homedir();
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
     ])
-    .sass('resources/sass/app.sass', 'public/css');
+    .sass('resources/sass/app.sass', 'public/css')
+    .browserSync({
+        proxy: 'https://' + domain,
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '-20rem'
+            }
+        },
+        host: domain,
+        open: 'external',
+        https: {
+            key: homedir + "/.config/valet/Certificates/" + domain + ".key",
+            cert: homedir + "/.config/valet/Certificates/" + domain + ".crt"
+        },
+    });
+
+mix.copy('resources/images', 'public/images');
 
 if (mix.inProduction()) {
     mix.version();
