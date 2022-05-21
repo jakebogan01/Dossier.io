678645d4badd1f5430f0f09c5822f56cf269fe12
Jake <jakebogan@Jakes-iMac.local>
2022-05-21 18:46:00 -0400

    home style still in progress

---
+++ resources/views/components/left-timeline.blade.php
@@ -0,0 +1,35 @@
+<div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+    <div class="order-1 w-5/12"></div>
+    <div class="z-20 flex items-center order-1 shadow-xl w-8 h-8 rounded-full">
+        <div class="mx-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-[1.3125rem] h-[1.3125rem]" viewBox="0 0 21 21">
+                <circle cx="10.5" cy="10.5" r="10.5" fill="#ffcf7b" />
+                <circle cx="4.5" cy="4.5" r="4.5" fill="#1c1f2d" transform="translate(6 6)" />
+            </svg>
+        </div>
+    </div>
+    <div class="order-1 w-5/12">
+        <div class="flex">
+            <div class="flex-1">
+                <h4 class="mt-2 text-2xl text-white font-bold text-right">
+                    A designer born
+                </h4>
+            </div>
+            <div class="min-w-[152px]"></div>
+        </div>
+        <div class="flex">
+            <div class="flex-1 pt-4">
+                <p class="text-[#B1B7D6] font-bold text-right">
+                    Yes, I was born as a 90’s kid.
+                    Started observing the world.
+                </p>
+            </div>
+            <div class="flex">
+                <div class="pl-[3.125rem] -mt-1.5 text-right">
+                    <span class="block font-black text-[2.75rem] text-[#FFCF7B] leading-none">1990</span>
+                    <span class="block text-sm font-bold text-base">JUNE</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/right-arrow-button.blade.php
@@ -0,0 +1,8 @@
+<button class="flex justify-center items-center bg-[#4046FF] w-[48px] h-[48px] group-hover:bg-[#575cff]" type="button">
+    <span aria-hidden="true">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7"/>
+        </svg>
+    </span>
+    <span class="sr-only">Skip to next slide page</span>
+</button>
