73911f79c52ef8f6fff0b905d3d8fef4812233fb
Jake Bogan <jakebogan01@gmail.com>
2022-05-20 23:11:02 -0400

    home styles still in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,17 +37,32 @@
 
         <main class="main_container max-w-2560 mx-auto">
 
-            <div class="flex py-[3.125rem] px-[1.5625rem] font-lato">
+            <div class="flex py-[3.125rem] px-[1.5625rem] font-lato" x-data="{ test: false }">
+
+                <nav class="mt-10 space-y-12 transition-all duration-300 delay-500 overflow-hidden" :class="{ 'w-[58px]': test, 'w-0': !test }" x-cloak="test">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="transition-all duration-300" :class="{ 'translate-y-0 opacity-100 delay-700': test, 'translate-y-10 opacity-0 delay-250': !test }" width="54.617" height="54.617" viewBox="0 0 54.617 54.617"><path d="M27.309,0A27.309,27.309,0,1,0,54.617,27.309,27.309,27.309,0,0,0,27.309,0Zm0,12.585L40.643,22.948V42.032H31.469V30.513h-8.32V42.034H13.974V22.948Z" fill="#fff"/></svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="transition-all duration-300" :class="{ 'translate-y-0 opacity-100 delay-800': test, 'translate-y-10 opacity-0 delay-200': !test }" width="54.617" height="54.617" viewBox="0 0 54.617 54.617"><path d="M33.039,22.116H27.578V16.654h5.462m0,27.309H27.578V27.578h5.462M30.309,3A27.309,27.309,0,1,0,57.617,30.309,27.309,27.309,0,0,0,30.309,3Z" transform="translate(-3 -3)" fill="#fff"/></svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="transition-all duration-300" :class="{ 'translate-y-0 opacity-100 delay-900': test, 'translate-y-10 opacity-0 delay-150': !test }" width="54.617" height="54.617" viewBox="0 0 54.617 54.617"><path d="M22.96,42.1a17.569,17.569,0,0,1-12.385-5.067L22.96,24.8V7.5a17.3,17.3,0,1,1,0,34.6Z" transform="translate(4.305 2.508)" fill="#fff"/><path d="M56.117,28.809A27.309,27.309,0,1,1,28.809,1.5,27.308,27.308,0,0,1,56.117,28.809Zm-4.965,0A22.343,22.343,0,1,1,28.809,6.465,22.343,22.343,0,0,1,51.152,28.809Z" transform="translate(-1.5 -1.5)" fill="#fff" fill-rule="evenodd"/></svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="transition-all duration-300" :class="{ 'translate-y-0 opacity-100 delay-1000': test, 'translate-y-10 opacity-0 delay-100': !test }" width="54.617" height="54.617" viewBox="0 0 54.617 54.617"><path d="M27.309,0A27.309,27.309,0,1,0,54.617,27.309,27.309,27.309,0,0,0,27.309,0ZM36.5,8.531a3.067,3.067,0,0,1,2.165.889l4.544,4.542a3.357,3.357,0,0,1-.188,4.735,3.356,3.356,0,0,1-4.735.19L33.74,14.345a3.357,3.357,0,0,1,.19-4.735,3.6,3.6,0,0,1,2.565-1.079Zm-7.489,6L38.1,23.619,23.328,38.384,14.24,29.3,29.008,14.533ZM11.882,31.016,21.468,40.6,9.482,43l2.4-11.985Z" fill="#fff"/></svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="transition-all duration-300" :class="{ 'translate-y-0 opacity-100 delay-1100': test, 'translate-y-10 opacity-0 delay-50': !test }" width="54.617" height="54.617" viewBox="0 0 54.617 54.617"><path d="M27.309,0A27.309,27.309,0,1,0,54.617,27.309,27.309,27.309,0,0,0,27.309,0ZM25.218,12.855l1.173,5.4c-3.438.762-6.6,1.751-6.343,5.591h4.04V38.231H11.917V24.789c0-10.682,7.847-11.552,13.3-11.934Zm16.3,0,1.177,5.4c-3.438.762-6.6,1.751-6.343,5.591H40.4V38.231H28.225V24.789c0-10.682,7.844-11.552,13.3-11.934Z" fill="#fff"/></svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" class="transition-all duration-300" :class="{ 'translate-y-0 opacity-100 delay-1200': test, 'translate-y-10 opacity-0': !test }" width="54.617" height="54.617" viewBox="0 0 54.617 54.617"><path d="M30.684,3.375A27.309,27.309,0,1,0,57.992,30.684,27.388,27.388,0,0,0,30.684,3.375Zm0,8.194a8.194,8.194,0,1,1-8.194,8.194,8.221,8.221,0,0,1,8.194-8.194Zm0,39.44A19.891,19.891,0,0,1,14.3,42.264C14.429,36.8,25.222,33.8,30.684,33.8S46.938,36.8,47.069,42.264A19.929,19.929,0,0,1,30.684,51.009Z" transform="translate(-3.375 -3.375)" fill="#fff"/></svg>
+                </nav>
+
                 <div class="flex-1 space-y-[3.125rem]">
                     <div class="flex relative bg-[#1C1F2D] px-[5.3125rem] pt-[2.1875rem] pb-[7.375rem] mx-[1.5625rem] overflow-hidden bg-no-repeat bg-right-top bg-auto" style="background-image: url('/images/svg/background-pattern.svg')">
                         <div class="flex-1">
                             <div class="mb-[4.375rem]">
 {{-- menu --}}
-                                <div class="w-[2.8125rem] space-y-1.5">
+                                <div class="w-[2.8125rem] space-y-1.5 cursor-pointer" @click="test = !test">
                                     <div class="bg-white h-[0.1875rem]"></div>
-                                    <div class="bg-white h-[0.1875rem] w-9"></div>
-                                    <div class="bg-white h-[0.1875rem] w-[1.6875rem]"></div>
-                                    <div class="bg-white h-[0.1875rem] w-4"></div>
+                                    <div class="bg-white h-[0.1875rem] w-9 transition-transform duration-300 delay-200" :class="{ 'translate-x-[9px]': test }"></div>
+                                    <div class="bg-white h-[0.1875rem] w-[1.6875rem] transition-transform  duration-300 delay-100" :class="{ 'translate-x-[18px]': test }"></div>
+                                    <div class="bg-white h-[0.1875rem] w-4 transition-transform duration-300" :class="{ 'translate-x-[29px]': test }"></div>
                                 </div>
                             </div>
                             <div class="text-[3.375rem] font-black text-white">
@@ -86,7 +101,7 @@
 {{-- users profile picture --}}
                         <div class="flex-1 pt-10 pl-[165px]">
                             <div class="flex items-center justify-center w-[30.0625rem] h-[30.0625rem] rounded-full" style="background-image: linear-gradient(to bottom right, #FFD279, #FFF659); box-shadow: 0 0 4.125rem #FFE26B;">
-                                <div class="w-[25.125rem] h-[25.125rem] bg-[#1C1F2D] rounded-full bg-no-repeat bg-center bg-cover" style="background-image: url('https://www.liveabout.com/thmb/FeWbkEye5kn7wmE96gVodlbFsig=/2121x1193/smart/filters:no_upscale()/GettyImages-638316888-58bd8bc93df78c353c5b8631.jpg')"></div>
+                                <div class="w-[25.125rem] h-[25.125rem] bg-[#1C1F2D] rounded-full bg-no-repeat bg-center bg-cover" style="background-image: url('https://avatars.githubusercontent.com/u/66225768?v=4')"></div>
                             </div>
                         </div>
                     </div>
@@ -564,7 +579,7 @@
                                     aria-labelledby="carousel-content-label"
                                     class="flex w-full overflow-hidden hover:overflow-x-auto snap-x snap-mandatory max-h-[550px] min-h-[550px] h-full">
 
-                                    <li x-bind="disableNextAndPreviousButtons" class="relative snap-start w-full shrink-0 flex flex-col items-center justify-center g-no-repeat bg-center bg-cover" role="option" style="background-image: url('https://www.liveabout.com/thmb/FeWbkEye5kn7wmE96gVodlbFsig=/2121x1193/smart/filters:no_upscale()/GettyImages-638316888-58bd8bc93df78c353c5b8631.jpg')">
+                                    <li x-bind="disableNextAndPreviousButtons" class="relative snap-start w-full shrink-0 flex flex-col items-center justify-center g-no-repeat bg-center bg-cover" role="option" style="background-image: url('https://avatars.githubusercontent.com/u/66225768?v=4')">
                                         <div class="absolute top-0 left-0 right-0 bottom-0 text-white leading-loose pt-32 pl-16" style="background-color: rgba(0,0,0,0.5)">
                                             <h3 class="flex items-center font-bold text-[23px] mb-6">
                                                 Art app
