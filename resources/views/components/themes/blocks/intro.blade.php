3e7a9033e6c9d44e068efa8356bfabbc029308d9
Jake <jakebogan@Jakes-iMac.local>
2022-05-27 18:48:29 -0400

    home styles still in progress

--- resources/views/components/themes/blocks/intro.blade.php
+++ resources/views/components/themes/blocks/intro.blade.php
@@ -69,7 +69,7 @@
         <div
             data-aos="zoom-in-right"
              data-aos-delay="400">
-            <a href="#">
+            <a href="#contact">
                 <button type="button" class="inline-flex justify-between items-center px-6 py-2 border border-transparent shadow-sm text-base font-black text-white bg-[#4046FF] sm:hover:bg-[#575cff] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 max-w-[200px] w-full tracking-widest">
                     LETS TALK
                     <svg xmlns="http://www.w3.org/2000/svg" class="ml-3 -mr-1 h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
