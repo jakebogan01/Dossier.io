8f663fa40fe384a38fdd2cdb82c1b1f4ee019ed4
Jake Bogan <jakebogan01@gmail.com>
2022-06-05 01:48:45 -0400

    created first 404 page

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,28 +1,44 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
+    <x-registration.authentication-card>
         <div class="mb-4 text-sm text-gray-600">
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
         </div>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.confirm') }}">
+        <x-registration.validation-errors />
+        <form
+            method="POST"
+            action="{{ route('password.confirm') }}"
+            class="space-y-6">
             @csrf
-
-            <div>
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" autofocus />
+            <div
+                data-aos="fade-up"
+                data-aos-delay="300">
+                <x-registration.label
+                    for="password"
+                    value="{{ __('Password') }}" />
+                <x-registration.input
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    autocomplete="current-password"
+                    style="padding: 10px 0 10px 20px;"
+                    required
+                    autofocus />
             </div>
-
-            <div class="flex justify-end mt-4">
-                <x-jet-button class="ml-4">
+            <div class="pt-2">
+                <x-registration.button
+                    data-aos="fade-up"
+                    data-aos-delay="350">
                     {{ __('Confirm') }}
-                </x-jet-button>
+                </x-registration.button>
+                <a
+                    href="/"
+                    class="w-full flex justify-center mt-4 py-2 px-4 border border-transparent rounded-md shadow-sm font-medium text-white bg-indigo-700 hover:bg-[#3273F6] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-colors"
+                    data-aos="fade-up"
+                    data-aos-delay="400">
+                    Cancel
+                </a>
             </div>
         </form>
-    </x-jet-authentication-card>
+    </x-registration.authentication-card>
 </x-guest-layout>
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -40,11 +40,11 @@
                     {{ __('Email Password Reset Link') }}
                 </x-registration.button>
                 <a
-                    href="/"
+                    href="{{ route('login') }}"
                     class="w-full flex justify-center mt-4 py-2 px-4 border border-transparent rounded-md shadow-sm font-medium text-white bg-indigo-700 hover:bg-[#3273F6] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-colors"
                     data-aos="fade-up"
                     data-aos-delay="400">
-                    Cancel
+                    Back
                 </a>
             </div>
         </form>
