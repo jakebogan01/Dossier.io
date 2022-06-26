b59ce9ff34d21f3dee6a6a54eb720062633aa805
Jake Bogan <jakebogan01@gmail.com>
2022-06-26 15:07:53 -0400

    hides portfolio to non users if not public

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -8,8 +8,14 @@ class ThemeController extends Controller
 {
     public function themeOne($id)
     {
-        $user = User::whereRelation('profile', 'slug', '!=', null)
-            ->whereRelation('profile', 'slug', $id)->first();
+        if (auth()->user()) {
+            $user = User::whereRelation('profile', 'slug', '!=', null)
+                ->whereRelation('profile', 'slug', $id)->first();
+        } else {
+            $user = User::whereRelation('profile', 'slug', '!=', null)
+                ->whereRelation('profile', 'settings->public', 'true')
+                ->whereRelation('profile', 'slug', $id)->first();
+        }
 
         if (is_null($user)) {
             return redirect()->route('home');
