e01b427c8e8092f51d6aa41cc04358e39068612c
Jake Bogan <jakebogan01@gmail.com>
2022-06-30 19:31:13 -0400

    dashboard styles updated

--- resources/views/livewire/dashboard-form.blade.php
+++ resources/views/livewire/dashboard-form.blade.php
@@ -8,7 +8,7 @@
     @if(count($currentUser->projects) >= 1)
         <div class="mt-6 space-y-4">
             @foreach($currentUser->projects as $project)
-                <div class="{{ ($currentUser->dark_mode) ? 'bg-[#742fa8] sm:hover:bg-[#8b3fc4]' : 'bg-white' }} group shadow-md sm:hover:shadow-sm rounded-lg sm:transition-all duration-300 ease-linear">
+                <div aria-roledescription="list of all projects" class="{{ ($currentUser->dark_mode) ? 'bg-[#742fa8] sm:hover:bg-[#8b3fc4]' : 'bg-white' }} group shadow-md sm:hover:shadow-sm rounded-lg sm:transition-all duration-300 ease-linear">
                     <div class="px-4 py-4 sm:px-6">
                         <div class="flex items-center justify-between">
                             <p class="{{ ($currentUser->dark_mode) ? 'text-white' : 'text-indigo-600' }} text-lg font-bold truncate cursor-default">
@@ -27,11 +27,11 @@
                             <div class="sm:flex">
                                 <a href="{{ $project->links['github'] }}" target="_blank" class="{{ ($currentUser->dark_mode) ? 'text-white sm:hover:text-indigo-300' : 'text-indigo-700 sm:text-gray-500 sm:hover:text-indigo-400' }} opacity-50 sm:group-hover:opacity-100 flex items-center text-sm sm:transition-opacity">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 mr-1 h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
-                                    Engineering
+                                    Project
                                 </a>
                                 <a href="{{ $project->links['code'] }}" target="_blank" class="{{ ($currentUser->dark_mode) ? 'text-white sm:hover:text-indigo-300' : 'text-indigo-700 sm:text-gray-500 sm:hover:text-indigo-400' }} opacity-50 sm:group-hover:opacity-100 mt-2 flex items-center text-sm sm:mt-0 sm:ml-6 sm:transition-opacity">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 mr-1 h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" /></svg>
-                                    Remote
+                                    Code
                                 </a>
                             </div>
                             <div class="{{ ($currentUser->dark_mode) ? 'text-white' : 'text-gray-900 sm:text-gray-500' }} mt-2 flex items-center text-sm opacity-50 sm:group-hover:opacity-100 sm:mt-0 cursor-default sm:transition-opacity">
