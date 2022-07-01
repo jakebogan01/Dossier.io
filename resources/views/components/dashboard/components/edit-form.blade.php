d4b3ab0ba551bc545d4c7fcfa0d2c349fb45811b
Jake Bogan <jakebogan01@gmail.com>
2022-06-30 22:06:27 -0400

    made toggle switch component

--- resources/views/components/dashboard/components/edit-form.blade.php
+++ resources/views/components/dashboard/components/edit-form.blade.php
@@ -95,21 +95,14 @@
                             </div>
                         </div>
                         <div class="pb-4 pt-8 flex items-center justify-between">
-                            <div class="flex flex-col">
+                            <x-dashboard.components.toggle-switch>
                                 <p class="{{ (auth()->user()->dark_mode) ? 'text-white' : 'text-gray-900' }} text-sm font-medium" id="privacy-option-1-label">
                                     Make project public
                                 </p>
                                 <p class="{{ (auth()->user()->dark_mode) ? 'text-gray-300' : 'text-gray-500' }} text-sm" id="privacy-option-1-description">
                                     Allow this project to be seen by the public.
                                 </p>
-                            </div>
-                            <div class="relative inline-block h-6 w-11 align-middle select-none sm:transition duration-200 ease-in">
-                                <input wire:model="make_public" type="checkbox" name="make_public" id="make_public" class="toggle-checkbox absolute block outline-none border border-white bg-white appearance-none cursor-pointer p-0 h-5 w-5 mt-0.5 mx-0.5 rounded-full sm:transition shadow transform ring-0 ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white" />
-                                <label for="make_public" class="{{ (auth()->user()->dark_mode) ? 'toggle-dark-label bg-black' : 'toggle-label bg-gray-200' }} block overflow-hidden h-6 w-11 rounded-full cursor-pointer sm:transition"></label>
-                                <label for="make_public" class="text-xs text-gray-700 sr-only">
-                                    make_public
-                                </label>
-                            </div>
+                            </x-dashboard.components.toggle-switch>
                         </div>
                     </div>
                 </div>
