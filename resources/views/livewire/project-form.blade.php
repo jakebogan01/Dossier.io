28bc615cb686da131511d79f6165e3b7bf0fb32e
Jake Bogan <jakebogan01@gmail.com>
2022-07-03 17:23:27 -0400

    small update

--- resources/views/livewire/project-form.blade.php
+++ resources/views/livewire/project-form.blade.php
@@ -102,11 +102,11 @@
                                          x-transition:leave-start="opacity-100 translate-y-0"
                                          x-transition:leave-end="opacity-0 translate-y-1">
                                         <div class="relative ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                            <div class="{{ (auth()->user()->dark_mode) ? 'bg-[#333333] text-white' : 'bg-white' }} flex space-x-4">
-                                                <button wire:click.prevent="show({{ $project->id }})" @click="edit = true; settings = false" type="button" class="sm:hover:text-[#4FAE9D] pl-4 py-2">
+                                            <div class="{{ (auth()->user()->dark_mode) ? 'bg-[#333333] text-white' : 'bg-white' }} flex justify-end space-x-4">
+                                                <button wire:click.prevent="show({{ $project->id }})" @click="edit = true; settings = false" type="button" class="sm:hover:text-[#4FAE9D] py-2">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" /></svg>
                                                 </button>
-                                                <button wire:click.prevent="delete({{ $project->id }})" @click="notify = true; settings = false" type="button" class="sm:hover:text-red-300 py-1">
+                                                <button wire:click.prevent="delete({{ $project->id }})" @click="notify = true; settings = false" type="button" class="sm:hover:text-red-300 pr-4 py-1">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
                                                 </button>
                                             </div>
