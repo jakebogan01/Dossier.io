4e3e89a65f03d1943dee1b7695b670a2a94079ec
Jake Bogan <jakebogan01@gmail.com>
2022-07-19 15:46:32 -0400

    portfolio url copy dashbaord fixed

--- resources/views/components/dashboard/components/portfolio-link.blade.php
+++
@@ -1,3 +0,0 @@
-<button aria-label="Copy Portfolio URL" type="button" x-data @click="$tooltip('Copied!')" class="usersPortfolio cursor-pointer" onclick="copyText()">
-    <span class="sr-only">https://mydossier.dev/portfolio</span><span class="{{ (auth()->user()->dark_mode) ? 'text-white sm:hover:text-blue-300' : 'text-[#3531B87F] sm:hover:text-[#3531B8]' }} block transform sm:hover:scale-90 sm:transition-all">/{{ $usersPortfolio }}</span>
-</button>
