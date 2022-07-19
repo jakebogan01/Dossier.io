be683910920f4be4c5e97904afb140c38155dff9
Jake Bogan <jakebogan01@gmail.com>
2022-07-19 15:43:17 -0400

    test: dashboard portfolio url copy

--- resources/views/components/dashboard/welcome.blade.php
+++ resources/views/components/dashboard/welcome.blade.php
@@ -42,8 +42,9 @@
                         Portfolio Link
                     </dt>
                     <dd class="flex mt-1 text-3xl font-semibold">
-                        @dd(auth()->user()->profile->slug)
-{{--                        <x-dashboard.components.portfolio-link></x-dashboard.components.portfolio-link>--}}
+                        <button aria-label="Copy Portfolio URL" type="button" x-data @click="$tooltip('Copied!')" class="usersPortfolio cursor-pointer" onclick="copyText()">
+                            <span class="sr-only">https://mydossier.dev/portfolio</span><span class="{{ (auth()->user()->dark_mode) ? 'text-white sm:hover:text-blue-300' : 'text-[#3531B87F] sm:hover:text-[#3531B8]' }} block transform sm:hover:scale-90 sm:transition-all">/auth()->user()->profile->slug</span>
+                        </button>
                     </dd>
                 </div>
             </div>
