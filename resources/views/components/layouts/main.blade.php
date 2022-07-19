4e8f3ed6463d94592f608a98b17f25823632c05c
Jake Bogan <jakebogan01@gmail.com>
2022-07-19 12:33:38 -0400

    adds beta warning

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -57,7 +57,22 @@
             }
         </style>
     </head>
-    <body class="antialiased bg-[#0F1119] h-full">
+    <body class="antialiased bg-[#0F1119] h-full" x-data="{ showWarning: true }">
+        <div x-cloak x-show="showWarning" class="relative bg-indigo-600">
+            <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                <div class="pr-16 sm:text-center sm:px-16">
+                    <p class="font-medium text-white">
+                        <span> Warning: This site is currently in Beta! </span>
+                    </p>
+                </div>
+                <div @click="showWarning = false" class="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
+                    <button type="button" class="flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white">
+                        <span class="sr-only">Dismiss</span>
+                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                </div>
+            </div>
+        </div>
         {{ $slot }}
         @livewireScripts
     </body>
